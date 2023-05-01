7b7dcc04d16e4622cca621a1b7a9e8bdf4a29676
Andrey <129365108+AndreyBurov0702@users.noreply.github.com>
2023-05-01 21:30:19 +0500

    Add files via upload

---
+++ DVDPlayer.java
@@ -0,0 +1,20 @@
+public class DVDPlayer {
+    boolean canRecord = false;
+
+    void recordDVD(){
+        System.out.println("идет запись DVD");
+    }
+    void playDVD(){
+        System.out.println("идет проигрывание DVD");
+    }
+}
+class DVDPlayerTestDrive{
+    public static void main(String[] args) {
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+        if (d.canRecord == true){
+            d.recordDVD();
+        }
+    }
+}
